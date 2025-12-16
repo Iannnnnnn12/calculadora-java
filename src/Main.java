cde15838b40a52833dba3f7913d44c198b16ecdc
Iannnnnnn12 <ian.gimeno@gracia.lasalle.cat>
2025-12-16 12:38:54 +0100

    Modelo menu para printar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
+
 public class Main {
+
+    private static Scanner escaner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        boolean menuOn = true;
+
+        do {
+            Menu();
+            int opcion =
+
+        }while (menuOn);
+
+
+
+
+
+
+
     }
+
+    private static void Menu(){
+        System.out.println("\n==============================");
+        System.out.println("     CALCULADORA (Consola)     ");
+        System.out.println("==============================");
+        System.out.println("1) Operaciones básicas (+ - * / %)");
+        System.out.println("2) Operaciones avanzadas (pow, sqrt, factorial)");
+        System.out.println("3) Historial");
+        System.out.println("0) Salir");
+        System.out.println("==============================");
+    }
+
+    private static void
 }
